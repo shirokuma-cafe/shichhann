33ff8f849f9f4b20b303920fb09febe6ea985dc2
Savannah Chhann <schhann327@gmail.com>
2024-09-04 17:26:18 -0400

    updated gallery with clickable full screen feature

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const images = ['Baku Spot copy 2.jpg', 'Bubble Finish copy.jpg', 'Chhann_IBP1_D1 copy.jpg', 'Christmas Promo Back (1) copy.jpg', 'Christmas Promo Front copy.jpg', 'Clock copy 2.jpg', 'Divide.jpg', 'DSC-0021.jpg', 'Emperor & White Tiger.jpg', 'FullSizeRender (2) copy.jpg', 'Halloween Promo Back copy.jpg', 'Halloween Promo Front copy.jpg', 'Hilly copy 2.jpg', 'Himawari.jpg', 'ILLO_Chhann_Stephany_image1 copy.jpg', 'Leaf copy 2.jpg', 'Link x Zelda TOTK.JPG', 'Luminary sheet.jpg', 'Milly copy.jpg', 'Oli Sticker Sheet Cutlines.jpg', 'OliBanner copy.jpg', 'OliNight-02.jpg', 'Pastword copy 3.jpg', 'Peace copy.jpg', 'Photo on Aug 20, 2021 at 12-09-31 PM copy.jpg', 'Photo on May 21, 2023 at 11-14-40 AM.jpg', 'Photo on Oct 16, 2023 at 9-01-50 AM.jpg', 'Photo on Sep 28, 2023 at 8-56-50 AM.jpg',  'Photos 2 on Aug 4, 2022 at 2-51-24 PM copy.jpg', 'Photos 3 on Mar 26, 2021 at 11-22-45 AM copy.jpg', 'Photos on Sep 8, 2022 at 7-56-59 AM copy.jpg', 'Ponytail.jpg', 'Ranma Finish copy.jpg', 'Red Panda.JPEG', 'Sleep 2 copy.jpg', 'Steampunk Shop.jpg', 'Tenki no Ko copy.jpg', 'The Temptation of Thanatos copy 3.jpg', 'Three of Us.jpg', 'Twitter Icon copy.jpg', 'Veronica.jpg'];
+const images = ['Baku Spot copy 2.jpg', 'Bubble Finish copy.jpg', 'Chhann_IBP1_D1 copy.jpg', 'Christmas Promo Back (1) copy.jpg', 'Christmas Promo Front copy.jpg', 'Clock copy 2.jpg', 'Darling dance-protected-intensity-LOW-V2 s.jpg', 'Divide.jpg', 'DSC-0021.jpg', 'Emperor & White Tiger.jpg', 'FullSizeRender (2) copy.jpg', 'Halloween Promo Back copy.jpg', 'Halloween Promo Front copy.jpg', 'Hilly copy 2.jpg', 'Himawari.jpg', 'ILLO_Chhann_Stephany_image1 copy.jpg', 'Leaf copy 2.jpg', 'Link x Zelda TOTK.JPG', 'Luminary sheet-protected-intensity-LOW-V2 s.jpg', 'Milly copy.jpg', 'Oli Sticker Sheet Cutlines.jpg', 'OliBanner copy.jpg', 'OliNight-02.jpg', 'Pastword copy 3.jpg', 'Peace copy.jpg', 'Photo on Aug 20, 2021 at 12-09-31 PM copy.jpg', 'Photo on May 21, 2023 at 11-14-40 AM.jpg', 'Photo on Oct 16, 2023 at 9-01-50 AM.jpg', 'Photo on Sep 28, 2023 at 8-56-50 AM.jpg',  'Photos 2 on Aug 4, 2022 at 2-51-24 PM copy.jpg', 'Photos 3 on Mar 26, 2021 at 11-22-45 AM copy.jpg', 'Photos on Sep 8, 2022 at 7-56-59 AM copy.jpg', 'Ponytail.jpg', 'Ranma Finish copy.jpg', 'Red Panda.JPEG', 'Sleep 2 copy.jpg', 'Steampunk Shop.jpg', 'Tenki no Ko copy.jpg', 'The Temptation of Thanatos copy 3.jpg', 'Three of Us.jpg', 'Twitter Icon copy.jpg', 'Veronica.jpg'];
 const gallery = document.querySelector('.gallery-images');
 
 function preloadImages(images) {
@@ -30,5 +30,14 @@ document.addEventListener('readystatechange', (event) => {
       gallery.style.visibility= 'visible';
     }, 1000);
     createGallery(images);
+    const galleryImages = document.querySelectorAll('.gallery img');
+    const imageFull = document.querySelector('.imageFull');
+    galleryImages.forEach((image) => {
+      image.addEventListener('click', () => {
+        imageFull.style.backgroundImage = `url('${image.src}')`;
+        imageFull.style.display = 'block';
+      });
+    })
   }
 });
+
